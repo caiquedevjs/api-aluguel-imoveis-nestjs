fac002f50e4387f1bb90578e5c6dd1507db0abdc
caiquedevjs <caiquemenezes1@outlook.com>
2024-05-20 18:02:12 -0300

    validações

--- src/Modules/Imovel/DTOS/imovel.dtos.ts
+++ src/Modules/Imovel/DTOS/imovel.dtos.ts
@@ -1,12 +1,24 @@
 /* eslint-disable prettier/prettier */
+import { IsBoolean, IsNumber, IsString } from "class-validator";
+
+/* eslint-disable prettier/prettier */
 export class ImovelDTOS{
+    @IsString()
     id? : string;
+    @IsString()
     type : string;
+    @IsString()
     description : string;
+    @IsNumber()
     room : number;
+    @IsNumber()
     garage : number;
+    @IsNumber()
     service_area : number;
+    @IsBoolean()
     availability : boolean;
+    @IsNumber()
     value : number;
+    @IsString()
     bar_code : string;
 }
--- src/Modules/Usuario/DTOS/usuario.dtos.ts
+++ src/Modules/Usuario/DTOS/usuario.dtos.ts
@@ -1,14 +1,23 @@
 /* eslint-disable prettier/prettier */
+import { IsNumber, IsString } from "class-validator"
+
+/* eslint-disable prettier/prettier */
 export class UsuarioDTOS{
+    @IsString()
     id? : string
+    @IsString()
     name : string
+    @IsString()
     mail : string
+    @IsString()
     password : string
     
 
 }
 
 export class valoresDTOS{
+    @IsNumber()
     valor_inicial : number
+    @IsNumber()
     valor_final : number
 }
